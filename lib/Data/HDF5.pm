d626d03ac944c867995fe7d0f9669a5853e0f73e
jvolkening <jdv@base2bio.com>
2018-09-12 12:49:09 -0500

    fix library link naming

--- lib/Data/HDF5.pm
+++ lib/Data/HDF5.pm
@@ -343,7 +343,7 @@ our %EXPORT_TAGS = (
 
 our @EXPORT_OK = ( @{ $EXPORT_TAGS{'all'} } );
 
-our $VERSION = '0.002';
+our $VERSION = '0.002001';
 
 require XSLoader;
 XSLoader::load('Data::HDF5', $VERSION);
